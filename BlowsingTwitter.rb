48b9dce05547ca3f6228957e825edc2bad5c4fb4
ShotaKinekawa <mmmicap.kinetch@gmail.com>
2021-02-11 16:03:14 +0900

    JavaScriptを併用して，自動いいね機能追加

--- BlowsingTwitter.rb
+++ BlowsingTwitter.rb
@@ -3,12 +3,13 @@ class BlowsingTwitter
 
     def initialize(user, pass)
 
+        #Chrome起動
         @driver = Selenium::WebDriver.for :chrome
-        @driver.manage.timeouts.implicit_wait = 60
         # 指定したドライバの要素が見つかるまでの待ち時間を指定
+        @driver.manage.timeouts.implicit_wait = 60
         
-        @driver.navigate.to "https://twitter.com/login"
         # URLを指定してアクセス
+        @driver.navigate.to "https://twitter.com/login"
         
         @driver.find_element(:xpath, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[1]/label/div/div[2]/div/input').send_keys(user)
         #user名入力
@@ -16,31 +17,55 @@ class BlowsingTwitter
         @driver.find_element(:xpath, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[2]/label/div/div[2]/div/input').send_keys(pass)
         #pass入力
         
-        @driver.find_element(:xpath, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[3]/div').click
         #loginボタンクリック
+        @driver.find_element(:xpath, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[3]/div').click
     end
 
     def tweet(msg)
-        @driver.find_element(:xpath, '//*[@id="react-root"]/div/div/div[2]/header/div/div/div/div[1]/div[3]/a/div').click
         #ツイートするボタンクリック
+        @driver.find_element(:xpath, '//*[@id="react-root"]/div/div/div[2]/header/div/div/div/div[1]/div[3]/a/div').click
         
-        @driver.find_element(:xpath, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[3]/div/div/div/div[1]/div/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div[2]/div').send_keys(msg)
         #メッセージ入力
+        @driver.find_element(:xpath, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[3]/div/div/div/div[1]/div/div/div/div/div[2]/div[1]/div/div/div/div/div/div/div/div/div/div[1]/div/div/div/div[2]/div').send_keys(msg)
         
-        @driver.find_element(:xpath, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[3]/div/div/div/div[1]/div/div/div/div/div[2]/div[4]/div/div/div[2]/div[4]').click
         #送信ボタンクリック    
+        @driver.find_element(:xpath, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[3]/div/div/div/div[1]/div/div/div/div/div[2]/div[4]/div/div/div[2]/div[4]').click
     end
     
     def search(query)
-        query = URI.encode_www_form_component(query)
         #パーセントエンコーディング
+        query = URI.encode_www_form_component(query)
 
-        @driver.navigate.to "https://twitter.com/search?q="+query+"&src=typed_query&f=live"
         #検索
+        @driver.navigate.to "https://twitter.com/search?q="+query+"&src=typed_query&f=live"
+    end
+
+    def fav_some_tweets(query)
+        search(query)
+
+        # 現在位置＋5000pxに縦スクロール
+        @driver.execute_script(%Q{window.scroll(0,window.scrollY + 5000);})
+
+        #5秒後に画面内の未ふぁぼツイートにふぁぼする
+        @driver.execute_script(%Q{
+            setTimeout(
+                function () {
+                    // いいね！の要素を取得
+                    var elems = document.body.querySelectorAll('div[data-testid="like"]');
+            
+                    // 取得できたいいね！の要素の回数分実行
+                    for (var i = 0; i < elems.length; i++) {
+            
+                        // いいね！ボタンをクリック
+                        elems[i].click();
+                    }         
+                }
+            , 5000);
+        })
     end
 
     def finish
-        @driver.quit
         # ドライバを閉じる
+        @driver.quit
     end
 end
