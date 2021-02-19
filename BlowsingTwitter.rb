0320dce27c3da55f79f8a15aab12136d18a8c26a
ShotaKinekawa <mmmicap.kinetch@gmail.com>
2021-02-19 21:25:46 +0900

    YARDの挿入

--- BlowsingTwitter.rb
+++ BlowsingTwitter.rb
@@ -1,10 +1,20 @@
+#
+# twitterをブラウザで開いて，自動でつぶやいたり，ふぁぼしますw
+#
 class BlowsingTwitter
     require 'selenium-webdriver'
 
+    #
+    # Chromeを開いて，twitterにログインします。
+    #
+    # @param [Strings] user ユーザー名
+    # @param [Strings] pass パスワード
+    #
     def initialize(user, pass)
 
-        #Chrome起動
+        #Chromeのドライバ
         @driver = Selenium::WebDriver.for :chrome
+
         # 指定したドライバの要素が見つかるまでの待ち時間を指定
         @driver.manage.timeouts.implicit_wait = 60
         
@@ -21,6 +31,12 @@ class BlowsingTwitter
         @driver.find_element(:xpath, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div[2]/form/div/div[3]/div').click
     end
 
+    #
+    # 引数で受け取ったメッセージをツイートします。
+    #
+    # @param [Strings] msg メッセージ
+    #
+    #
     def tweet(msg)
         #ツイートするボタンクリック
         @driver.find_element(:xpath, '//*[@id="react-root"]/div/div/div[2]/header/div/div/div/div[1]/div[3]/a/div').click
@@ -32,30 +48,44 @@ class BlowsingTwitter
         @driver.find_element(:xpath, '//*[@id="layers"]/div[2]/div/div/div/div/div/div[2]/div[2]/div/div[3]/div/div/div/div[1]/div/div/div/div/div[2]/div[4]/div/div/div[2]/div[4]').click
     end
     
+    #
+    # 受け取ったクエリ文でtwitterの検索をします。
+    #
+    # @param [Strings] query twitterのクエリ文
+    # @param [Boolean] only_following フォロー中アカウントのつぶやきに限定して検索するか（デフォルトはfalse）
+    #
     def search(query, only_following = false)
+        
         #パーセントエンコーディング
         query = URI.encode_www_form_component(query)
-
+        
         #url 
         url = "https://twitter.com/search?q="+query+"&src=typed_query&f=live"
-
+        
         #following限定検索か？
         if only_following
             url += "&pf=on"
         end
-
-        #検索
+        
+        #検索（ツイートは新しい順に表示）
         @driver.navigate.to url
-
+        
         #5秒待つ
         sleep 5
-
+        
         #更新する
         @driver.navigate.refresh
-
+        
     end
-
+    
+    #
+    # 引数で受け取ったクエリ文で検索し，最新のツイートをいくつかふぁぼします。
+    #
+    # @param [Strings] query twitterのクエリ文
+    # @param [Boolean] only_following フォロー中アカウントのつぶやきに限定して検索するか（デフォルトはfalse）
+    #
     def fav_some_tweets(query, only_following = false)
+
         search(query, only_following)
 
         # 現在位置＋5000pxに縦スクロール
@@ -79,6 +109,9 @@ class BlowsingTwitter
         })
     end
 
+    #
+    # 画面を閉じます
+    #
     def finish
         # ドライバを閉じる
         @driver.quit
